d03e2c4903b0741c9b7be464966df6d3960cbf96
vineet416 <vineetpatel468@gmail.com>
2025-06-17 12:47:26 +0530

    Updated app.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -80,8 +80,8 @@ class DataTransformation:
 
             self.utils.save_object(file_path= preprocessor_path, obj= preprocessor)
 
-            train_arr = np.c[X_train_scaled, np.array(y_train)]
-            test_arr = np.c[X_train_scaled, np.array(y_test)]
+            train_arr = np.c_[X_train_scaled, np.array(y_train)]
+            test_arr = np.c_[X_train_scaled, np.array(y_test)]
 
             return (train_arr, test_arr, preprocessor_path)
         
