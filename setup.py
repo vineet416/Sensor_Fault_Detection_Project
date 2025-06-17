1374bd8e622b0ba0b40df98252848b796f8b2d70
vineet416 <vineetpatel468@gmail.com>
2025-06-17 12:56:29 +0530

    Updated requirements and setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 from typing import List
 
-HYPHEN_E_DOT = "-e ."
+HYPHEN_E_DOT = "-e."
 def get_requirements(file_path: str) -> List[str]:
     """
     This function will return the list of requirements
